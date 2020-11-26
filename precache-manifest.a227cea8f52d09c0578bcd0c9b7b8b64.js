3b8a9040a4178860a7523de3d15614a8874f9e44
Shea McKinney <sherakama@gmail.com>
2020-11-26 15:32:25 -0800

    Updates

---
+++ precache-manifest.a227cea8f52d09c0578bcd0c9b7b8b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28aace6a9d6e4231db809560f4d6ea51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08859a6178c92070bfd8",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "07e389101a694264a032",
+    "url": "./static/js/2.b499e329.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b499e329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08859a6178c92070bfd8",
+    "url": "./static/js/main.2be4362f.chunk.js"
+  },
+  {
+    "revision": "72a4c78863131998715e",
+    "url": "./static/js/runtime-main.13dd729e.js"
+  }
+]);
