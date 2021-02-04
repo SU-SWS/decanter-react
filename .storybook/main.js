403e00f8a3ab16280835c3a9993cc15ce02e2fbc
Sherakama <sherakama@gmail.com>
2021-02-03 23:27:25 -0800

    Progress. (#3)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,18 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../storybook/**/*.stories.mdx",
+    "../storybook/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    "@storybook/addon-essentials",
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    // https://storybook.js.org/addons/@whitespace/storybook-addon-html
+    "@whitespace/storybook-addon-html",
+    // https://storybook.js.org/addons/storybook-addon-designs
+    "storybook-addon-designs",
+    // https://storybook.js.org/addons/storybook-dark-mode
+    "storybook-dark-mode"
   ]
 }
