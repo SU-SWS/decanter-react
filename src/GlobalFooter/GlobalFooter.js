4a7eef4bfb402c22f89d2b212459a6daebaaae68
Yvonne Tang <ytang2@stanford.edu>
2021-02-12 01:50:12 -0800

    Finish global footer styles

--- src/GlobalFooter/GlobalFooter.js
+++ src/GlobalFooter/GlobalFooter.js
@@ -31,34 +31,34 @@ export const GlobalFooter = ({ classes = {}, ...props }) => {
   }
 
   return (
-    <div className={classnames('su-global-footer su-rs-py-1 su-link-white hover:su-link-white focus:su-link-white', levers.wrapper)}>
-      <div
-        className='su-cc'
-        title='Common Stanford resources'
-      >
-        <div className='su-flex su-flex-col lg:sm-flex-row'>
-          <a className='su-logo su-type-3' href='https://www.stanford.edu'>
-            Stanford
-            <br />
-            University
-          </a>
+    <div
+      className={classnames('su-global-footer su-rs-py-1 su-text-white su-link-white hover:su-link-white focus:su-link-white', levers.wrapper)}
+    >
+      <div className='su-cc su-flex su-flex-col lg:su-flex-row' title='Common Stanford resources'>
+        <div className='su-text-center su-mt-5 su-mb-9'>
+          <a className='su-logo su-type-3' href='https://www.stanford.edu'>Stanford<br />University</a>
         </div>
-        <div className='su-global-footer__content su-text-left sm:su-text-center lg:su-text-left su-flex-grow'>
-          <nav aria-label='global footer menu' className='su-flex su-mb-10'>
-            <ul className='su-global-footer__menu su-global-footer__menu--global'>
-              <li>
+        <div className='lg:su-pl-45 xl:su-pl-50 su-text-left sm:su-text-center lg:su-text-left su-flex-grow'>
+          <nav
+            aria-label='global footer menu'
+            className='su-flex su-flex-row sm:su-flex-col su-justify-center sm:su-items-center lg:su-items-start su-mb-10 su-link-no-underline hover:su-link-underline focus:su-link-underline'
+          >
+            <ul
+              className='su-list-unstyled su-mb-10 sm:su-mb-4 su-mr-19 sm:su-mr-0 su-p-0 su-text-15 md:su-text-17 2xl:su-text-18 su-flex su-flex-col sm:su-flex-row'
+            >
+              <li className='sm:su-mr-10 md:su-mr-20 lg:su-mr-27'>
                 <a href='https://www.stanford.edu'>
                   Stanford Home
                   {SrOnlyLabel}
                 </a>
               </li>
-              <li>
+              <li className='sm:su-mr-10 md:su-mr-20 lg:su-mr-27'>
                 <a href='https://visit.stanford.edu/plan/'>
                   Maps &amp; Directions
                   {SrOnlyLabel}
                 </a>
               </li>
-              <li>
+              <li className='sm:su-mr-10 md:su-mr-20 lg:su-mr-27'>
                 <a href='https://www.stanford.edu/search/'>
                   Search Stanford
                   {SrOnlyLabel}
@@ -71,66 +71,50 @@ export const GlobalFooter = ({ classes = {}, ...props }) => {
                 </a>
               </li>
             </ul>
-            <ul className='su-global-footer__menu su-global-footer__menu--policy'>
-              <li>
-                <a
-                  href='https://www.stanford.edu/site/terms/'
-                  title='Terms of use for sites'
-                >
+            <ul
+              className='su-list-unstyled su-mb-10 sm:su-mb-0 su-ml-19 sm:su-ml-0 su-p-0 su-text-15 sm:su-text-14 md:su-text-15 xl:su-text-16 su-flex su-flex-col sm:su-flex-row sm:su-link-regular'
+            >
+              <li className='sm:su-mr-10 md:su-mr-20 lg:su-mr-27'>
+                <a href='https://www.stanford.edu/site/terms/' title='Terms of use for sites'>
                   Terms of Use
                   {SrOnlyLabel}
                 </a>
               </li>
-              <li>
-                <a
-                  href='https://www.stanford.edu/site/privacy/'
-                  title='Privacy and cookie policy'
-                >
+              <li className='sm:su-mr-10 md:su-mr-20 lg:su-mr-27'>
+                <a href='https://www.stanford.edu/site/privacy/' title='Privacy and cookie policy'>
                   Privacy
                   {SrOnlyLabel}
                 </a>
               </li>
-              <li>
-                <a
-                  href='https://uit.stanford.edu/security/copyright-infringement'
-                  title='Report alleged copyright infringement'
-                >
+              <li className='sm:su-mr-10 md:su-mr-20 lg:su-mr-27'>
+                <a href='https://uit.stanford.edu/security/copyright-infringement' title='Report alleged copyright infringement'>
                   Copyright
                   {SrOnlyLabel}
                 </a>
               </li>
-              <li>
-                <a
-                  href='https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4'
-                  title='Ownership and use of Stanford trademarks and images'
-                >
+              <li className='sm:su-mr-10 md:su-mr-20 lg:su-mr-27'>
+                <a href='https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4' title='Ownership and use of Stanford trademarks and images'>
                   Trademarks
                   {SrOnlyLabel}
                 </a>
               </li>
-              <li>
-                <a
-                  href='http://exploredegrees.stanford.edu/nonacademicregulations/nondiscrimination/'
-                  title='Non-discrimination policy'
-                >
+              <li className='sm:su-mr-10 md:su-mr-20 lg:su-mr-27'>
+                <a href='http://exploredegrees.stanford.edu/nonacademicregulations/nondiscrimination/' title='Non-discrimination policy'>
                   Non-Discrimination
                   {SrOnlyLabel}
                 </a>
               </li>
               <li>
-                <a
-                  href='https://www.stanford.edu/site/accessibility'
-                  title='Report web accessibility issues'
-                >
+                <a href='https://www.stanford.edu/site/accessibility' title='Report web accessibility issues'>
                   Accessibility
                   {SrOnlyLabel}
                 </a>
               </li>
             </ul>
           </nav>
-          <div className='su-global-footer__copyright'>
-            <span>&copy; Stanford University.</span>
-            <span>&nbsp; Stanford, California 94305.</span>
+          <div className='su-text-13 sm:su-text-14 su-text-center lg:su-text-left'>
+            <span className='su-whitespace-no-wrap'>&copy; Stanford University.</span>
+            <span className='su-whitespace-no-wrap'>&nbsp; Stanford, California 94305.</span>
           </div>
         </div>
       </div>
