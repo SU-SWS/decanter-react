fb10c344f0a24752661484e4ec35ba3f95d79e7a
Shea McKinney <sherakama@gmail.com>
2021-02-15 14:36:00 -0800

    fixup! stuff.

--- src/Alert/Alert.levers.js
+++ src/Alert/Alert.levers.js
@@ -8,9 +8,9 @@ export const alertTypes = ['info', 'warning', 'error', 'success']
 /**
  * Styles for light text
  */
-export const lightText = classnames('su-text-white su-link-color-black-true hover:su-link-no-underline')
+export const lightText = classnames('su-text-white hover:su-link-no-underline')
 
 /**
  * Styles for dark text
  */
-export const darkText = classnames('su-text-black')
+export const darkText = classnames('su-text-black su-link-black-true hover:su-link-no-underline')
