b46a77720e64dc2e104922220f636d9679502485
Yvonne Tang <ytang2@stanford.edu>
2021-02-16 13:02:24 -0800

    Temporary quick test to avoid build error because of failed test

--- src/GlobalFooter/GlobalFooter.test.js
+++ src/GlobalFooter/GlobalFooter.test.js
@@ -1,7 +1,18 @@
-import { GlobalFooter } from '.'
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
+import { GlobalFooter } from './GlobalFooter'
+
+// Component is a component.
 describe('GlobalFooter', () => {
+  // Is a component with valid syntax.
   it('is truthy', () => {
     expect(GlobalFooter).toBeTruthy()
+  })
+
+  // Default is rendered.
+  it('renders the GlobalFooter in the default state', () => {
+    render(<GlobalFooter />)
   })
 })
--- src/IdentityBar/IdentityBar.test.js
+++ src/IdentityBar/IdentityBar.test.js
@@ -1,7 +1,18 @@
-import { IdentityBar } from './IdentityBar';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
+import { IdentityBar } from './IdentityBar'
+
+// Component is a component.
 describe('IdentityBar', () => {
+  // Is a component with valid syntax.
   it('is truthy', () => {
-    expect(IdentityBar).toBeTruthy();
-  });
-});
+    expect(IdentityBar).toBeTruthy()
+  })
+
+  // Default is rendered.
+  it('renders the IdentityBar in the default state', () => {
+    render(<IdentityBar />)
+  })
+})
