14df42040ddf8cf192f52716e9d9ac160116234e
Shea McKinney <sherakama@gmail.com>
2021-02-17 12:00:15 -0800

    fixup! jest no-cache.

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "verbose": true,
+  "testPathIgnorePatterns": [
+    "/node_modules/",
+    "/docs/"
+  ],
+  "testRegex": "(/test/.*|\\.(test|spec))\\.(js|jsx)$",
+  "moduleFileExtensions": [
+    "js",
+    "jsx",
+    "json"
+  ],
+  "moduleDirectories": [
+    "node_modules",
+    "src"
+  ]
+}
