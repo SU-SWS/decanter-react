3160f37dfccbd2e93dfc5a0f6b93e61a2f1aa5b6
Yvonne Tang <ytang2@stanford.edu>
2021-02-17 01:23:49 -0800

    Add logo component

---
+++ src/Logo/Logo.levers.js
@@ -0,0 +1,9 @@
+/**
+ * Default colors for the color prop.
+ */
+export const LogoColors = ['cardinal-red', 'black', 'white'];
+
+/**
+ * The three different logo types.
+ */
+export const LogoTypes = ['short', 'full', 'stacked'];
---
+++ src/Logo/Logo.stories.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Logo } from './Logo';
+import { LogoColors, LogoTypes } from "./Logo.levers";
+
+export default {
+  title: 'Stanford Identity/Logo',
+  component: Logo,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: LogoColors
+      }
+    },
+    type: {
+      control: {
+        type: 'select',
+        options: LogoTypes
+      }
+    },
+  }
+};
+
+// Set up an Alert Template.
+const LogoTemplate = ({ children, ...rest }) => <Logo {...rest} />;
+
+export const Red = LogoTemplate.bind({});
+Red.args = {
+  color: 'cardinal-red',
+  type: 'short',
+  className: 'su-type-3'
+};
+
+export const Black = LogoTemplate.bind({});
+Black.args = {
+  color: 'black',
+  type: 'short',
+  className: 'su-type-3'
+};
+
+export const White = LogoTemplate.bind({});
+White.args = {
+  color: 'white',
+  type: 'short',
+  className: 'su-type-3'
+};
+
+export const Full = LogoTemplate.bind({});
+Full.args = {
+  color: 'cardinal-red',
+  type: 'full',
+  className: 'su-type-3'
+};
+
+export const Stacked = LogoTemplate.bind({});
+Stacked.args = {
+  color: 'cardinal-red',
+  type: 'stacked',
+  className: 'su-type-3'
+};
