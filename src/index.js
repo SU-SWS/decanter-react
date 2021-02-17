f818b9fa27a2c3d7d47e5139e423f0f162b23052
Yvonne Tang <ytang2@stanford.edu>
2021-02-17 02:00:56 -0800

    export Logo

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { Alert } from './Alert/Alert';
 import { Button } from './Button/Button';
 import { IdentityBar } from './IdentityBar/IdentityBar';
 import { GlobalFooter } from './GlobalFooter/GlobalFooter';
-import { Hero } from './Hero';
+import { Logo } from './Logo/Logo';
 import { SrOnlyLabel } from './SrOnlyLabel/SrOnlyLabel';
 import { StyledLink } from './StyledLink';
 
-export { Alert, Button, IdentityBar, GlobalFooter, Hero, SrOnlyLabel, StyledLink };
+export { Alert, Button, IdentityBar, GlobalFooter, Logo, SrOnlyLabel, StyledLink };
