379d9a60eca2ebbbeb49b1d8c25ad13ca6ef533c
Shea McKinney <sherakama@gmail.com>
2021-02-18 01:35:11 -0800

    fixup! more stuff.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'decanter-react.js',
-    libraryTarget: 'umd',
+    libraryTarget: 'commonjs',
   },
   module: {
     rules: [
