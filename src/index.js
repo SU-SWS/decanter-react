0a5eea430ad3a60126e19a48da01317eedbb7429
Shea McKinney <sherakama@gmail.com>
2021-02-18 12:05:50 -0800

    fixup! bugs!

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import { Alert } from './Alert/Alert';
-import { Button } from './Button/Button';
-import { IdentityBar } from './IdentityBar/IdentityBar';
-import { GlobalFooter } from './GlobalFooter/GlobalFooter';
-import { Logo } from './Logo/Logo';
-import { SrOnlyText } from './SrOnlyText/SrOnlyText';
+// import { Alert } from './Alert/Alert';
+// import { Button } from './Button/Button';
+// import { IdentityBar } from './IdentityBar/IdentityBar';
+// import { GlobalFooter } from './GlobalFooter/GlobalFooter';
+// import { Logo } from './Logo/Logo';
+// import { SrOnlyText } from './SrOnlyText/SrOnlyText';
+
+// module.exports = {
+//   'Alert' : Alert,
+//   'Button' : Button,
+//   'IdentityBar' : IdentityBar,
+//   'GlobalFooter' : GlobalFooter,
+//   'Logo' : Logo,
+//   'SrOnlyText' : SrOnlyText
+// }
+
+import React from 'react'
+
+const GlobalFooter = (props) => {
+  return (
+    <h1 {...props}>I am groot</h1>
+  )
+}
 
 module.exports = {
-  'Alert' : Alert,
-  'Button' : Button,
-  'IdentityBar' : IdentityBar,
-  'GlobalFooter' : GlobalFooter,
-  'Logo' : Logo,
-  'SrOnlyText' : SrOnlyText
+  'GlobalFooter': GlobalFooter
 }
