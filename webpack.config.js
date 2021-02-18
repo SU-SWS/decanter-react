f4ae6be566e3591f8290023cc2ce691c8e114a97
Shea McKinney <sherakama@gmail.com>
2021-02-18 00:10:46 -0800

    ADAPT-000: Add commonjs packaging.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'decanterReact',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
