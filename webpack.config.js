9c1dd96ef0810b4ebfcea3bbcb262b9e64abca40
Shea McKinney <sherakama@gmail.com>
2021-02-18 00:30:11 -0800

    fixup! stuff.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'umd',
-    library: 'decanterReact',
+    library: {
+      root: 'decanter',
+      amd: 'decanter-react',
+      commonjs: 'decanter-react'
+    },
   },
   module: {
     rules: [
@@ -21,4 +25,19 @@ module.exports = {
     ]
   },
   plugins: [new CleanWebpackPlugin()],
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    },
+    'react-hero-icon': 'react-hero-icon'
+  }
 };
