5482878fc5553ecd04e51bf18012b8ab2290c519
Yvonne Tang <ytang2@stanford.edu>
2021-02-24 09:46:46 -0800

    update decanter to v7 alpha and export Grid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { Alert } from './Alert/Alert';
 import { Button } from './Button/Button';
 import { Container } from './Container/Container';
-import { IdentityBar } from './IdentityBar/IdentityBar';
 import { GlobalFooter } from './GlobalFooter/GlobalFooter';
+import { Grid } from './Grid/Grid';
+import { IdentityBar } from './IdentityBar/IdentityBar';
 import { Logo } from './Logo/Logo';
 import { SrOnlyText } from './SrOnlyText/SrOnlyText';
 import { StyledLink } from './StyledLink';
 
-export { Alert, Button, Container, IdentityBar, GlobalFooter, Logo, SrOnlyText, StyledLink };
+export { Alert, Button, Container, GlobalFooter, Grid, IdentityBar, Logo, SrOnlyText, StyledLink };
