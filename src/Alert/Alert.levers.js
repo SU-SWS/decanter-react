90544c92ebc1f283cf63986b4ca93a1022f3ea32
Sherakama <sherakama@gmail.com>
2021-02-23 22:02:37 -0800

    Update Alert.levers.js

--- src/Alert/Alert.levers.js
+++ src/Alert/Alert.levers.js
@@ -1,5 +1,3 @@
-import clsx from 'clsx';
-
 /**
  * Default Types for the type prop.
  */
@@ -8,9 +6,9 @@ export const alertTypes = ['info', 'warning', 'error', 'success']
 /**
  * Styles for light text
  */
-export const lightText = clsx('su-text-white hover:su-link-no-underline')
+export const lightText = 'su-text-white hover:su-link-no-underline'
 
 /**
  * Styles for dark text
  */
-export const darkText = clsx('su-text-black su-link-black-true hover:su-link-no-underline')
+export const darkText = 'su-text-black su-link-black-true hover:su-link-no-underline'
