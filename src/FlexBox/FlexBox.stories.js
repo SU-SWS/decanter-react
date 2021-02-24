c57dd67db7f5192cad393c216d84f2aaf4de4276
Yvonne Tang <ytang2@stanford.edu>
2021-02-24 12:09:42 -0800

    flexbox stories

--- src/FlexBox/FlexBox.stories.js
+++ src/FlexBox/FlexBox.stories.js
@@ -8,7 +8,7 @@ export default {
   argTypes: {
     direction: {
       control: {
-        type: 'select',
+        type: 'inline-radio',
         options: flexDirection
       }
     },
@@ -38,7 +38,7 @@ export default {
     },
     alignItems: {
       control: {
-        type: 'inline-radio',
+        type: 'select',
         options: flexAlignItems
       }
     },
@@ -48,15 +48,36 @@ export default {
 const FlexBoxTemplate = ({ children, ...rest }) => {
   return (
     <FlexBox {...rest}>
-      <div className="su-w-1/6 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">1</div>
-      <div className="su-w-1/6 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">2</div>
+      <div className="su-w-1/3 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">1</div>
+      <div className="su-w-1/6 su-text-center su-bg-plum su-type-3 su-font-bold su-p-10">2</div>
       <div className="su-w-1/6 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">3</div>
+    </FlexBox>
+  )
+}
+
+const FlexBoxMultiTemplate = ({ children, ...rest }) => {
+  return (
+    <FlexBox {...rest}>
+      <div className="su-w-1/3 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">1</div>
+      <div className="su-w-1/6 su-text-center su-bg-plum su-type-3 su-font-bold su-p-10">2</div>
+      <div className="su-w-1/6 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">3</div>
+      <div className="su-w-1/3 su-text-center su-bg-plum su-type-3 su-font-bold su-p-10">4</div>
+      <div className="su-w-1/6 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">5</div>
+      <div className="su-w-1/6 su-text-center su-bg-plum su-type-3 su-font-bold su-p-10">6</div>
     </FlexBox>
   )
 }
 
 export const Default = FlexBoxTemplate.bind({});
 Default.args = {
-  className: 'su-bg-foggy-light'
+  className: 'su-bg-foggy-light su-text-white'
 };
-Default.storyName = 'FlexBox'
+Default.storyName = 'Single Row'
+
+export const MultipleRows = FlexBoxMultiTemplate.bind({});
+MultipleRows.args = {
+  wrap: 'wrap',
+  justifyContent: 'start',
+  className: 'su-bg-foggy-light su-text-white'
+};
+MultipleRows.storyName = 'Multiple Rows with Wrapping'
