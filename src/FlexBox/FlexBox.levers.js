cfc53323f5f51bbcdd3de3928b873e0e2f82d53a
Yvonne Tang <ytang2@stanford.edu>
2021-02-24 11:17:11 -0800

    FlexBox and levers

---
+++ src/FlexBox/FlexBox.levers.js
@@ -0,0 +1,31 @@
+/**
+ * Flex Directions.
+ */
+export const flexDirection = ['row', 'row-reverse', 'col', 'col-reverse'];
+
+/**
+ * Flex Wrap options.
+ */
+export const flexWrap = ['wrap', 'wrap-reverse', 'nowrap'];
+
+/**
+ * Flex Justify Content options.
+ */
+export const flexJustifyContent = ['start', 'end', 'center', 'space-between', 'space-around', 'space-evenly'];
+
+/**
+ * Flex Justify Items options.
+ */
+export const flexJustifyItems = ['auto', 'start', 'end', 'center', 'stretch'];
+
+/**
+ * Flex Align Content options.
+ */
+export const flexAlignContent = ['start', 'end', 'center', 'space-between', 'space-around', 'space-evenly'];
+
+/**
+ * Flex Align Items options.
+ */
+export const flexAlignItems = ['flex-start', 'flex-end', 'center', 'baseline', 'stretch'];
+
+
