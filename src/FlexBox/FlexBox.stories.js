327de69ba2bd43e50a6095a0ded7980ffa114aaf
Yvonne Tang <ytang2@stanford.edu>
2021-02-24 20:06:14 -0800

    More flexbox stories

--- src/FlexBox/FlexBox.stories.js
+++ src/FlexBox/FlexBox.stories.js
@@ -56,7 +56,7 @@ const FlexBoxTemplate = ({ children, ...rest }) => {
     <FlexBox {...rest}>
       <div className="su-w-1/3 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">1</div>
       <div className="su-w-1/6 su-text-center su-bg-plum su-type-3 su-font-bold su-p-10">2</div>
-      <div className="su-w-1/6 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">3</div>
+      <div className="su-w-1/3 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">3</div>
     </FlexBox>
   )
 }
@@ -66,7 +66,7 @@ const FlexBoxMultiTemplate = ({ children, ...rest }) => {
     <FlexBox {...rest}>
       <div className="su-w-1/3 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">1</div>
       <div className="su-w-1/6 su-text-center su-bg-plum su-type-3 su-font-bold su-p-10">2</div>
-      <div className="su-w-1/6 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">3</div>
+      <div className="su-w-1/3 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">3</div>
       <div className="su-w-1/4 su-text-center su-bg-plum su-type-3 su-font-bold su-p-10">4</div>
       <div className="su-w-1/5 su-text-center su-bg-spirited su-type-3 su-font-bold su-p-10">5</div>
       <div className="su-w-1/6 su-text-center su-bg-plum su-type-3 su-font-bold su-p-10">6</div>
@@ -76,14 +76,42 @@ const FlexBoxMultiTemplate = ({ children, ...rest }) => {
 
 export const Default = FlexBoxTemplate.bind({});
 Default.args = {
+  gap: true,
   className: 'su-bg-foggy-light su-text-white'
 };
 Default.storyName = 'Single Row'
 
+export const SpaceBetween = FlexBoxTemplate.bind({});
+SpaceBetween.args = {
+  gap: true,
+  justifyContent: 'space-between',
+  className: 'su-bg-foggy-light su-text-white'
+};
+SpaceBetween.storyName = 'Single Row with Space Between Items'
+
 export const MultipleRows = FlexBoxMultiTemplate.bind({});
 MultipleRows.args = {
   wrap: 'wrap',
+  gap: true,
   justifyContent: 'start',
   className: 'su-bg-foggy-light su-text-white'
 };
-MultipleRows.storyName = 'Multiple Rows with Wrapping'
+MultipleRows.storyName = 'Multiple Rows'
+
+export const MultipleRowsCenter = FlexBoxMultiTemplate.bind({});
+MultipleRowsCenter.args = {
+  wrap: 'wrap',
+  gap: true,
+  justifyContent: 'center',
+  className: 'su-bg-foggy-light su-text-white'
+};
+MultipleRowsCenter.storyName = 'Multiple Rows with Centered Items'
+
+export const ColumnCenter = FlexBoxMultiTemplate.bind({});
+ColumnCenter.args = {
+  direction: 'col',
+  gap: true,
+  alignItems: 'center',
+  className: 'su-bg-foggy-light su-text-white'
+};
+ColumnCenter.storyName = 'Centered Items in Single Column'
