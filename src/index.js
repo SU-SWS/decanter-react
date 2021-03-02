bde3915017c3c32948b6df6528b1d2a3aba59e64
Yvonne Tang <ytang2@stanford.edu>
2021-03-01 17:44:15 -0800

    Add test for Heading

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import { Alert } from './Alert/Alert';
 import { Button } from './Button/Button';
 import { Container } from './Container/Container';
 import { FlexBox } from './FlexBox/FlexBox';
+import { Heading } from './Heading/Heading';
 import { GlobalFooter } from './GlobalFooter/GlobalFooter';
 import { Grid } from './Grid/Grid';
 import { IdentityBar } from './IdentityBar/IdentityBar';
+import { LocalFooter } from './LocalFooter/LocalFooter';
 import { Lockup } from './Lockup/Lockup';
 import { Logo } from './Logo/Logo';
 import { SrOnlyText } from './SrOnlyText/SrOnlyText';
 import { StyledLink } from './StyledLink';
 
-export { Alert, Button, Container, FlexBox, GlobalFooter, Grid, IdentityBar, Lockup, Logo, SrOnlyText, StyledLink };
+export { Alert, Button, Container, FlexBox, GlobalFooter, Grid, Heading, IdentityBar, LocalFooter, Lockup, Logo, SrOnlyText, StyledLink };
