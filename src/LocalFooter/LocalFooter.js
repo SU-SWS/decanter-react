a89ccef010f73c62027ed23170b25053e7140b7b
Yvonne Tang <ytang2@stanford.edu>
2021-03-02 23:23:40 -0800

    login button icon; classes for different parts in local footer

--- src/LocalFooter/LocalFooter.js
+++ src/LocalFooter/LocalFooter.js
@@ -6,6 +6,7 @@ import { Grid } from '../Grid/Grid';
 import { FlexBox } from "../FlexBox/FlexBox";
 import { FlexCell } from "../FlexCell/FlexCell";
 import { Lockup } from "../Lockup/Lockup";
+import Icon from 'react-hero-icon';
 import clsxd from 'clsx-dedupe';
 
 /**
@@ -13,7 +14,7 @@ import clsxd from 'clsx-dedupe';
  * This usually sits above the Global Footer.
  *
  */
-export const LocalFooter = ({ className, children, ...props }) => {
+export const LocalFooter = ({ classes = {}, children, ...props }) => {
   const levers = {};
 
   // Levers
@@ -57,20 +58,21 @@ export const LocalFooter = ({ className, children, ...props }) => {
   }
 
   return (
-    <Container width='site' className={clsxd('su-local-footer su-rs-pt-4 su-rs-pb-5', levers.wrapper, className)}>
-      <FlexBox direction='col' gap={true} className='md:su-flex-row su-rs-mb-2' justifyContent={'space-between'}>
+    <Container width='site' className={clsxd('su-local-footer su-rs-pt-4 su-rs-pb-5', levers.wrapper, classes.wrapper)}>
+      <FlexBox direction='col' gap={true} className={clsxd('md:su-flex-row su-rs-mb-2', classes.header)} justifyContent={'space-between'}>
         <FlexCell grow={true}>
           <Lockup color={levers.lockup} line1={props.line1} url={props.url} />
         </FlexCell>
         {props.hasButton && (
           <FlexCell grow={false}>
-            <a href={props.buttonUrl} className='su-inline-block su-bg-digital-red su-text-white su-text-18 hocus:su-text-white hocus:su-bg-archway-dark su-py-9 su-px-20 su-no-underline su-font-regular hover:su-underline focus:su-underline su-rounded su-shadow-md'>
+            <a href={props.buttonUrl} className={clsxd('su-inline-block su-bg-digital-red su-text-white su-text-18 hocus:su-text-white hocus:su-bg-archway-dark su-py-9 su-px-20 su-no-underline su-font-regular hover:su-underline focus:su-underline su-rounded su-shadow-md', classes.button)}>
               {props.buttonText ?? 'Web Login'}
+              <Icon icon='lock-closed' type='solid' className={clsxd('su-inline-block su-h-08em su-w-08em su-ml-4 su--mt-2', classes.icon)}/>
             </a>
           </FlexCell>
         )}
       </FlexBox>
-      <Grid xs={1} md={2} xl={4} gap={true} className={clsxd('su-text-18 su-link-regular', levers.link, className)}>
+      <Grid xs={1} md={2} xl={4} gap={true} className={clsxd('su-text-18 su-link-regular', levers.link, classes.content)}>
         {children}
       </Grid>
     </Container>
@@ -121,14 +123,36 @@ LocalFooter.propTypes = {
     PropTypes.string
   ]),
 
-  /**
-   * Custom CSS classes, e.g., to control position
-   */
-  className: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.array,
-    PropTypes.object
-  ])
+  // Custom CSS classes
+  classes: PropTypes.shape(
+    {
+      wrapper: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object,
+        PropTypes.array
+      ]),
+      header: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object,
+        PropTypes.array
+      ]),
+      button: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object,
+        PropTypes.array
+      ]),
+      icon: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object,
+        PropTypes.array
+      ]),
+      content: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object,
+        PropTypes.array
+      ]),
+    }
+  ),
 };
 
 // Default Props.
