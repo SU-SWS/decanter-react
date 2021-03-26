6decca989bea214fcd9bcdbb38671fc617560362
Yvonne Tang <ytang2@stanford.edu>
2021-03-25 21:46:39 -0700

    ADAPT-2142: Dismiss Button as its own component and Alert tweaks (#22)

---
+++ src/DismissButton/DismissButton.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { DismissButton } from './DismissButton';
+
+// Component is a component.
+describe('DismissButton', () => {
+  // Is a component with valid syntax.
+  it('is truthy', () => {
+    expect(DismissButton).toBeTruthy();
+  });
+
+  // Default is rendered.
+  it('renders the Button in the default state', () => {
+    render(<DismissButton text='Test Text' />);
+    screen.getByText('Test Text'); // full string match
+  });
+});
