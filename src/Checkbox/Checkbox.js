2965b99c21790c833a7d4837ca052fb7241c469d
Elena <elena.cracea@gmail.com>
2021-09-15 21:15:33 -0600

    ADAPT-3119: Remove important for the checkbox classes

--- src/Checkbox/Checkbox.js
+++ src/Checkbox/Checkbox.js
@@ -36,7 +36,7 @@ export const Checkbox = React.forwardRef(
       switch (size) {
         case "small":
           levers.containerSizeOptions = "su-w-[34px] su-h-[34px]";
-          levers.boxSizeOptions = "!su-w-[20px] !su-h-[20px]";
+          levers.boxSizeOptions = "su-w-[20px] su-h-[20px]";
           levers.textSizeOptions = "su-text-16";
           break;
 
@@ -64,17 +64,17 @@ export const Checkbox = React.forwardRef(
         >
           <input
             type="checkbox"
-            className={`!su-border-black-50 su-rounded !su-border-2
-            focus-visible:!su-ring-offset-0 focus-visible:!su-ring-8 focus-visible:!su-border-digital-blue
-            hocus:!su-ring-offset-0 hocus:!su-ring-8
-            focus-visible:!su-ring-digital-blue-light focus-visible:!su-ring-opacity-50
+            className={`su-border-black-50 su-rounded su-border-2
+            focus-visible:su-ring-offset-0 focus-visible:su-ring-8 focus-visible:su-border-digital-blue
+            hocus:su-ring-offset-0 hocus:su-ring-8
+            focus-visible:su-ring-digital-blue-light focus-visible:su-ring-opacity-50
             focus-visible:su-bg-digital-blue-light focus-visible:su-bg-opacity-50
-            focus-visible:!su-border-digital-blue focus-visible:!su-border-opacity-50 focus-visible:!su-border-[3px]
-            focus:!su-ring-digital-blue-light focus:!su-ring-opacity-50 focus:su-bg-digital-blue-light
-            focus:su-bg-opacity-50  focus:!su-border-digital-blue focus:!su-border-opacity-50 focus:!su-border-[3px]
-            hover:!su-ring-digital-blue-light hover:!su-ring-opacity-20 hover:su-bg-digital-blue-light
-            hover:!su-border-digital-blue hover:!su-border-opacity-70 hover:su-bg-opacity-20
-            checked:!su-ring-2 checked:!su-border-digital-blue
+            focus-visible:su-border-digital-blue focus-visible:su-border-opacity-50 focus-visible:su-border-[3px]
+            focus:su-ring-digital-blue-light focus:su-ring-opacity-50 focus:su-bg-digital-blue-light
+            focus:su-bg-opacity-50 focus:su-border-digital-blue focus:su-border-opacity-50 focus:su-border-[3px]
+            hover:su-ring-digital-blue-light hover:su-ring-opacity-20 hover:su-bg-digital-blue-light
+            hover:su-border-digital-blue hover:su-border-opacity-70 hover:su-bg-opacity-20
+            checked:su-ring-2 checked:su-border-digital-blue
              ${levers.boxSizeOptions}`}
             value={props.value}
             name={props.name}
