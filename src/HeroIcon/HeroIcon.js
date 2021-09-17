87dc7706575e1c052b3a33eae0692c0c940ff047
rebeccahongsf <rebeccahongsf@gmail.com>
2021-09-16 22:34:09 -0700

    ADAPT-3128 Rewrite heroIconOption levers; WIP Resolve render issue when selecting different icon prop types

--- src/HeroIcon/HeroIcon.js
+++ src/HeroIcon/HeroIcon.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { dcnb } from "cnbuilder";
 import { VideoCameraIcon, MicrophoneIcon } from "@heroicons/react/outline";
 import {
@@ -10,73 +11,93 @@ import {
   PlayIcon,
 } from "@heroicons/react/solid";
 import { SrOnlyText } from "../SrOnlyText/SrOnlyText";
+import { heroIconOptions } from "./HeroIcon.levers";
 
-const HeroIcon = ({ iconType, srText, isAnimate, className, ...props }) => {
-  const heroIconMap = {
-    video: {
-      heroicon: VideoCameraIcon,
-      baseStyle: "su-w-08em su-mt-[-0.2em]",
-      animate: "group-hocus:su-translate-x-02em",
-    },
-    play: {
-      heroicon: PlayIcon,
-      baseStyle: "su-w-08em su-ml-7 su--mt-3",
-      animate: "group-hocus:su-translate-x-02em",
-    },
-    podcast: {
-      heroicon: MicrophoneIcon,
-      baseStyle: "su-w-08em su-mt-[-0.25em]",
-      animate: "group-hocus:su-translate-x-02em",
-    },
-    external: {
-      heroicon: ArrowRightIcon,
-      baseStyle: "su-w-08em su-ml-02em su--rotate-45 group-hocus:su--rotate-45",
-      animate:
-        "group-hocus:su-translate-x-01em group-hocus:su--translate-y-01em",
-    },
-    "arrow-right": {
-      heroicon: ArrowRightIcon,
-      baseStyle: "su-w-08em su-ml-03em su--mt-02em",
-      animate: "group-hocus:su-translate-x-02em",
-    },
-    download: {
-      heroicon: DownloadIcon,
-      baseStyle: "su-w-08em su-ml-4 su--mt-3",
-      animate: "group-hocus:su-translate-y-02em",
-    },
-    email: {
-      heroicon: MailIcon,
-      baseStyle: "su-w-08em su-ml-7 su--mt-2",
-      animate: "group-hocus:su-translate-x-02em",
-    },
-    "chevron-down": {
-      heroicon: ChevronDownIcon,
-      baseStyle: "su-w-[1.1em] su-ml-4 su--mt-3",
-      animate: "group-hocus:su-translate-y-02em",
-    },
-    "chevron-right": {
-      heroicon: ChevronRightIcon,
-      baseStyle: "su-w-1em su-ml-02em su--mt-4",
-      animate: "group-hocus:su-translate-x-02em",
-    },
-  };
+export const HeroIcon = ({ icon, srText, isAnimate, className, ...props }) => {
+  // Defaults & Variables.
+  // ---------------------------------------------------------------------------
 
-  const Icon = heroIconMap[iconType].heroicon;
+  let heroicon = ArrowRightIcon;
+  let baseStyle = "su-w-08em su-ml-03em su--mt-02em";
+  let animate = "group-hocus:su-translate-x-02em";
+
+  // Levers
+  // ---------------------------------------------------------------------------
+  if (icon && heroIconOptions[icon]) {
+    switch (icon) {
+      case "video":
+        heroicon = VideoCameraIcon;
+        baseStyle = "su-w-08em su-mt-[-0.2em]";
+        animate = "group-hocus:su-translate-x-02em";
+        break;
+
+      case "play":
+        heroicon = PlayIcon;
+        baseStyle = "su-w-08em su-ml-7 su--mt-3";
+        animate = "group-hocus:su-translate-x-02em";
+        break;
+
+      case "podcast":
+        heroicon = MicrophoneIcon;
+        baseStyle = "su-w-08em su-mt-[-0.25em]";
+        animate = "group-hocus:su-translate-x-02em";
+        break;
+
+      case "external":
+        heroicon = "ArrowRightIcon";
+        baseStyle =
+          "su-w-08em su-ml-02em su--rotate-45 group-hocus:su--rotate-45";
+        animate =
+          "group-hocus:su-translate-x-01em group-hocus:su--translate-y-01em";
+        break;
+
+      case "arrow-narrow-right":
+        heroicon = ArrowRightIcon;
+        baseStyle = "su-w-08em su-ml-03em su--mt-02em";
+        animate = "group-hocus:su-translate-x-02em";
+        break;
+
+      case "download":
+        heroicon = DownloadIcon;
+        baseStyle = "su-w-08em su-ml-4 su--mt-3";
+        animate = "group-hocus:su-translate-y-02em";
+        break;
+
+      case "email":
+        heroicon = MailIcon;
+        baseStyle = "su-w-08em su-ml-7 su--mt-2";
+        animate = "group-hocus:su-translate-x-02em";
+        break;
+
+      case "chevron-down":
+        heroicon = ChevronDownIcon;
+        baseStyle = "su-w-[1.1em] su-ml-4 su--mt-3";
+        animate = "group-hocus:su-translate-y-02em";
+        break;
+
+      case "chevron-right":
+        heroicon = ChevronRightIcon;
+        baseStyle = "su-w-1em su-ml-02em su--mt-4";
+        animate = "group-hocus:su-translate-x-02em";
+        break;
+
+      default:
+      // none
+    }
+  }
+
+  const IconElement = heroicon;
   let animateStyle = "";
 
   if (isAnimate) {
-    animateStyle = dcnb("su-transform-gpu", heroIconMap[iconType].animate);
+    animateStyle = dcnb("su-transform-gpu", animate);
   }
 
-  const heroIconStyle = dcnb(
-    "su-transition",
-    heroIconMap[iconType].baseStyle,
-    animateStyle
-  );
+  const heroIconStyle = dcnb("su-transition", baseStyle, animateStyle);
 
   return (
     <>
-      <Icon
+      <IconElement
         aria-hidden="true"
         className={dcnb(heroIconStyle, className)}
         {...props}
@@ -86,4 +107,31 @@ const HeroIcon = ({ iconType, srText, isAnimate, className, ...props }) => {
   );
 };
 
-export default HeroIcon;
+HeroIcon.propTypes = {
+  /**
+   * Hero Icon options
+   */
+  icon: PropTypes.oneOf(heroIconOptions),
+
+  /**
+   * Screen Reader Text
+   */
+  srText: PropTypes.string,
+
+  /**
+   * Is the icon animated?
+   */
+  isAnimate: PropTypes.bool,
+
+  /**
+   * CSS Class names.
+   */
+  className: PropTypes.string,
+};
+
+// Default Props.
+// -----------------------------------------------------------------------------
+HeroIcon.defaultProps = {
+  icon: "arrow-narrow-right",
+  isAnimate: true,
+};
