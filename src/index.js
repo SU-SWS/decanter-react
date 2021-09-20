cc6fc7b697775f81001255776c5ff154043481c6
rebeccahongsf <rebeccahongsf@gmail.com>
2021-09-20 07:46:02 -0700

    Resolve merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { Alert } from "./Alert/Alert";
-import { Button } from "./Button/Button";
-import { Card } from "./Card/Card";
-import { Container } from "./Container/Container";
-import { CtaButton } from "./CtaButton/CtaButton";
-import { CtaLink } from "./CtaLink/CtaLink";
-import { DismissButton } from "./DismissButton/DismissButton";
-import { FlexBox } from "./FlexBox/FlexBox";
-import { FlexCell } from "./FlexCell/FlexCell";
-import { Heading } from "./Heading/Heading";
-import { GlobalFooter } from "./GlobalFooter/GlobalFooter";
-import { Grid } from "./Grid/Grid";
-import { GridCell } from "./GridCell/GridCell";
-import { IdentityBar } from "./IdentityBar/IdentityBar";
-import { InlineError } from "./InlineError/InlineError";
-import { InlineValid } from "./InlineValid/InlineValid";
-import { Label } from "./Label/Label";
-import { LocalFooter } from "./LocalFooter/LocalFooter";
-import { Lockup } from "./Lockup/Lockup";
-import { Logo } from "./Logo/Logo";
-import { Poster } from "./Poster/Poster";
-import { Skiplink } from "./Skiplink/Skiplink";
-import { SrOnlyText } from "./SrOnlyText/SrOnlyText";
-import { TextField } from "./TextField/TextField";
-=======
 import { Alert } from './Alert/Alert';
 import { Button } from './Button/Button';
 import { Card } from './Card/Card';
@@ -38,13 +12,16 @@ import { GlobalFooter } from './GlobalFooter/GlobalFooter';
 import { Grid } from './Grid/Grid';
 import { GridCell } from './GridCell/GridCell';
 import { IdentityBar } from './IdentityBar/IdentityBar';
+import { InlineError } from './InlineError/InlineError';
+import { InlineValid } from './InlineValid/InlineValid';
+import { Label } from './Label/Label';
 import { LocalFooter } from './LocalFooter/LocalFooter';
 import { Lockup } from './Lockup/Lockup';
 import { Logo } from './Logo/Logo';
 import { Poster } from './Poster/Poster';
 import { Skiplink } from './Skiplink/Skiplink';
 import { SrOnlyText } from './SrOnlyText/SrOnlyText';
->>>>>>> main
+import { TextField } from './TextField/TextField';
 
 export {
   Alert,
