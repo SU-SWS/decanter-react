30f696dcb72c15a132094b272dbb2ed96f469b38
rebeccahongsf <rebeccahongsf@gmail.com>
2021-09-20 15:16:58 -0700

    ADAPT-3717 Adjust heroicon jest test

--- src/HeroIcon/HeroIcon.test.js
+++ src/HeroIcon/HeroIcon.test.js
@@ -1,10 +1,23 @@
-import React from "react";
-import HeroIcon from "./HeroIcon";
+/**
+ * @jest-environment jsdom
+ */
 
-// Component is a component.
-describe("HeroIcon", () => {
-  // Is a component with valid syntax.
-  it("is truthy", () => {
-    expect(HeroIcon).toBeTruthy();
-  });
-});
+ import React from "react";
+ import { render, screen } from "@testing-library/react";
+ import "@testing-library/jest-dom/extend-expect";
+ import { HeroIcon } from "./HeroIcon";
+ 
+ // Component is a component.
+ describe("HeroIcon", () => {
+   // Is a component with valid syntax.
+   it("is truthy", () => {
+     expect(HeroIcon).toBeTruthy();
+   });
+ 
+   // Default is rendered.
+   it("renders the Button in the default state", () => {
+     render(<HeroIcon text="Test Text" />);
+     screen.getByText("Test Text"); // full string match
+   });
+ });
+ 
