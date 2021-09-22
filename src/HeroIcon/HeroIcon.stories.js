ed5fa349e71b50b3c7df1f95319b7600ba016472
rebeccahongsf <rebeccahongsf@gmail.com>
2021-09-22 11:35:48 -0700

    ADAPT-3717 Add Hero Icon stories

--- src/HeroIcon/HeroIcon.stories.js
+++ src/HeroIcon/HeroIcon.stories.js
@@ -21,10 +21,52 @@ export const Default = HeroIconTemplate.bind({});
 Default.args = {
   icon: 'arrow-narrow-right',
 };
-Default.storyName = 'Default';
+Default.storyName = 'Arrow Right Icon';
 
 export const Video = HeroIconTemplate.bind({});
 Video.args = {
   icon: 'video',
 };
-Video.storyName = 'Video';
+Video.storyName = 'Video Icon';
+
+export const Play = HeroIconTemplate.bind({});
+Play.args = {
+  icon: 'play',
+};
+Play.storyName = 'Play Icon';
+
+export const Podcast = HeroIconTemplate.bind({});
+Podcast.args = {
+  icon: 'podcast',
+};
+Podcast.storyName = 'Podcast Icon';
+
+export const External = HeroIconTemplate.bind({});
+External.args = {
+  icon: 'external',
+};
+External.storyName = 'External Icon';
+
+export const Download = HeroIconTemplate.bind({});
+Download.args = {
+  icon: 'download',
+};
+Download.storyName = 'Download Icon';
+
+export const Email = HeroIconTemplate.bind({});
+Email.args = {
+  icon: 'email',
+};
+Email.storyName = 'Email Icon';
+
+export const ChevronDown = HeroIconTemplate.bind({});
+ChevronDown.args = {
+  icon: 'chevron-down',
+};
+ChevronDown.storyName = 'Chevron Down Icon';
+
+export const ChevronRight = HeroIconTemplate.bind({});
+ChevronRight.args = {
+  icon: 'chevron-right',
+};
+ChevronRight.storyName = 'Chevron Right Icon';
