58735ad76be23101df9830c2645fd0123e27f572
rebeccahongsf <rebeccahongsf@gmail.com>
2021-10-07 14:47:02 -0700

    ADAPT-3821 Fixup jest test

--- src/Tooltip/Tooltip.test.js
+++ src/Tooltip/Tooltip.test.js
@@ -2,21 +2,21 @@
  * @jest-environment jsdom
  */
 
-import React from "react";
-import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import React from "react";
+import { Tooltip } from "./Tooltip";
 
-import { InlineError } from "./Tooltip";
 
 // Component is a component.
 describe("InlineError", () => {
   // Is a component with valid syntax.
   it("is truthy", () => {
-    expect(InlineError).toBeTruthy();
+    expect(Tooltip).toBeTruthy();
   });
 
   // Default is rendered.
   it("renders the InlineError in the default state", () => {
-    render(<InlineError />);
+    render(<Tooltip />);
   });
 });
