de7ff45e03f8e52c20033e1909bf5e97d15b2acf
rebeccahongsf <rebeccahongsf@gmail.com>
2021-10-07 14:04:00 -0700

    ADAPT-3821 Tweak tooltip styles

--- src/Tooltip/Tooltip.js
+++ src/Tooltip/Tooltip.js
@@ -4,9 +4,9 @@ import React from 'react';
 export const Tooltip = ({ className, children, tooltipText }) => (
   <div className="su-relative su-flex su-items-center su-group">
     {children}
-    <div className="su-absolute su-top-0 su-hidden su-group-hover:su-flex su-flex-col su-items-center su-mt-26">
-      <div className="su-w-08em su-h-08em su--mb-2 su-rotate-45 su-bg-black" />
-      <span className="su-relative su-z-10 su-p-2 su-text-18 su-leading-none su-text-white su-whitespace-no-wrap su-bg-black su-shadow-lg">
+    <div className="su-absolute su-top-0 su-hidden group-hover:su-flex su-flex-col su-items-center su-mt-20 su-w-fit">
+      <div className="su-w-05em su-h-05em su--mb-5 su-rotate-45 su-bg-cool-grey" />
+      <span className="su-relative su-z-10 su-text-14 su-rounded su-leading-none su-text-white su-whitespace-no-wrap su-bg-cool-grey su-shadow-lg su-px-18 su-pb-18 su-pt-12 su-w-[50ch]">
         {tooltipText}
       </span>
     </div>
